db30cbed2cfbb34fc8e674be15670ecf72a6adf9
smenard <floogles71@gmail.com>
2025-12-16 16:21:43 +0100

    fix: update include paths in Makefile and header files

--- src/lib/list/list.h
+++ src/lib/list/list.h
@@ -6,7 +6,7 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 13:10:00 by smenard           #+#    #+#             */
-/*   Updated: 2025/12/16 15:47:25 by smenard          ###   ########.fr       */
+/*   Updated: 2025/12/16 16:21:10 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define LIST_H
 
 # include "lib_defines.h"
-# include "../lib.h"
+# include "lib.h"
 
 /* ft_lstnew.c */
 t_list	*ft_lstnew(int value, size_t *size);
