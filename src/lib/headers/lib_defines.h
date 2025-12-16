448f81500db29870bf31ed555fd0c5762a1c1f80
smenard <floogles71@gmail.com>
2025-12-16 11:25:44 +0100

    feat: lib defines

--- src/lib/headers/lib_defines.h
+++ src/lib/headers/lib_defines.h
@@ -6,11 +6,18 @@
 /*   By: smenard <smenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 11:18:22 by smenard           #+#    #+#             */
-/*   Updated: 2025/12/16 11:18:37 by smenard          ###   ########.fr       */
+/*   Updated: 2025/12/16 11:25:28 by smenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIB_DEFINES_H
 # define LIB_DEFINES_H
+
+typedef struct s_list
+{
+	struct s_list	*next;
+	struct s_list	*prev;
+	int				value;
+}					t_list;
 
 #endif
